8c14cb7a7e4a11223ea35eedc39fd2d07dc6d17d
kira1607 <kira16071981@gmail.com>
2021-08-03 17:52:55 +0300

    Task4

---
+++ addressbook-web-tests-new/addressbook-web-tests-new/ContactData.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+
+{
+    class ContactData
+
+    {
+        private string firstname;
+        private string middlename = "";
+        private string lastname;
+        private string nickname = "";
+        private string title = "";
+        private string company = "";
+        private string address = "";
+        private string home = "";
+        private string mobile = "";
+        private string work = "";
+        private string fax = "";
+        private string email = "";
+        private string email2 = "";
+        private string email3 = "";
+        private string homepage = "";
+        private string address2 = "";
+        private string phone2 = "";
+        private string notes = "";
+        private string bday = "";
+        private string bmonth = "";
+        private string byear = "";
+        private string aday = "";
+        private string amonyh = "";
+        private string ayear = "";
+
+      public ContactData(string firstname, string lastname)
+    {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+        public string FirstName
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+    }
+}
