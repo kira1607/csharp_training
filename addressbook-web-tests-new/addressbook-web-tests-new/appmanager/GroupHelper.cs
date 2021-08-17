c7aa2e618f369febe76c3f18c3f4a87ab5250bf1
kira1607 <kira16071981@gmail.com>
2021-08-17 13:31:52 +0300

    task 6_1

--- addressbook-web-tests-new/addressbook-web-tests-new/appmanager/GroupHelper.cs
+++ addressbook-web-tests-new/addressbook-web-tests-new/appmanager/GroupHelper.cs
@@ -31,14 +31,39 @@ namespace WebAddressbookTests
             return this;
 
         }
-        public GroupHelper Remove(int v)
+
+        public GroupHelper Remove(int p)
         {
            manager.Navigator.GoToGroupsPage();
-            SelectGroup(1);
+            SelectGroup(p);
             RemoveGroup();
             ReturntoGroupPage();
             return this;
         }
+        public GroupHelper Modify(int p, GroupData newData)
+        {
+            manager.Navigator.GoToGroupsPage();
+
+            SelectGroup(p);
+            InitGroupModification();
+            FillGroupForm(newData);
+            SubmitGroupModification();
+            ReturntoGroupPage();
+            return this;
+        }
+
+        public GroupHelper SubmitGroupModification()
+        {
+            driver.FindElement(By.Name("update")).Click();
+            return this;
+        }
+
+        public GroupHelper InitGroupModification()
+        {
+            driver.FindElement(By.Name("edit")).Click();
+            return this;
+        }
+
         public GroupHelper RemoveGroup()
                 {
                     driver.FindElement(By.Name("delete")).Click();
---
+++ addressbook-web-tests-new/addressbook-web-tests-new/tests/GroupModificationTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WebAddressbookTests.tests
+
+    {
+    [TestFixture]
+
+    public class GroupModificationTests: TestBase
+    {
+        [Test]
+        public void GroupModificationTest()
+    {
+            GroupData newData = new GroupData("test11");
+            newData.Header = "test22";
+            newData.Footer = "test33";
+
+            app.Groups.Modify(1, newData);
+
+        }
+    }
+
+}
